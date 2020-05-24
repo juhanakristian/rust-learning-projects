6043d85f601e844d78f6a178220f6052de2ad543
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-05-24 21:42:37 +0300

    Fibonacci cleanup

--- p5_fibonacci_sequence/src/main.rs
+++ p5_fibonacci_sequence/src/main.rs
@@ -2,27 +2,22 @@ use std::io;
 use std::vec;
 
 fn fibonacci(n: usize) -> Vec<u64> {
-    if n == 1 {
+    if n == 0 {
+        return vec![];
+    } else if n == 1 {
         return vec![1];
-    } else if n == 2 {
-        return vec![1, 1];
     }
 
     let mut sequence: Vec<u64> = vec![1, 1];
-    let mut i = 2;
-    loop {
+    for i in 2..n {
         sequence.push(sequence[i - 2] + sequence[i - 1]);
-        i += 1;
-
-        if i >= n {
-            break;
-        }
     }
+
     return sequence;
 }
 
 fn main() {
-    println!("Please insert number between 1 and 93");
+    println!("Please insert number between 0 and 93");
 
     let mut number = String::new();
     io::stdin()
@@ -31,21 +26,11 @@ fn main() {
 
     let number: usize = number.trim().parse().expect("Please type a number!");
 
-    if number < 1 || number > 93 {
+    if number > 93 {
         println!("Invalid number!");
         return;
     }
 
     let sequence: Vec<u64> = fibonacci(number);
-    let mut i = 0;
-    loop {
-        print!("{}", sequence[i]);
-        i += 1;
-        if i >= sequence.len() {
-            break;
-        }
-
-        print!(",");
-    }
-    print!("\n");
+    println!("{:?}", sequence);
 }
