787f4da0e2a98c6d952aa71dc21d20a34f7785e1
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-07-29 17:57:14 +0300

    Working on problem 9

--- p9_urlencoder/src/main.rs
+++ p9_urlencoder/src/main.rs
@@ -1,17 +1,59 @@
 use std::io;
 use std::io::Write;
 
+pub struct Reserved {
+    character: char,
+    encoded: &'static str
+}
+
+impl PartialEq<char> for Reserved {
+    fn eq(&self, other: &char) -> bool {
+        return self.character == *other;
+    }
+}
+
 fn percent_encode(input: &str) -> String{
     let mut encoded = String::new();
-    const reserved: &str = "!*'();:@&=+$,/?#[]";
+    // const reserved: &str = "!*'();:@&=+$,/?#[]";
+    const unreserved: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~";
+
+    let reserved = vec![
+        Reserved {character: '!', encoded: "21"},
+        Reserved {character: '*', encoded: "21"},
+        Reserved {character: '\'', encoded: "21"},
+        Reserved {character: '(', encoded: "21"},
+        Reserved {character: ')', encoded: "21"},
+        Reserved {character: ';', encoded: "21"},
+        Reserved {character: ':', encoded: "21"},
+        Reserved {character: '@', encoded: "21"},
+        Reserved {character: '&', encoded: "21"},
+        Reserved {character: '=', encoded: "21"},
+        Reserved {character: '+', encoded: "21"},
+        Reserved {character: '$', encoded: "21"},
+        Reserved {character: ',', encoded: "21"},
+        Reserved {character: '/', encoded: "21"},
+        Reserved {character: '?', encoded: "21"},
+        Reserved {character: '#', encoded: "21"},
+        Reserved {character: '[', encoded: "21"},
+        Reserved {character: ']', encoded: "21"},
+    ];
 
     for i in 0..input.len() {
         let character = input.chars().nth(i).unwrap();
-        match reserved.find(character) {
+        // Handle allowed characters
+        if unreserved.contains(character) {
+            encoded.insert(i, character)
+        }
+
+        
+        let mut iter = reserved.iter();
+        match iter.find(|x| **x == character) {
             Some(index) => {
                 // Replace with percent encoded value
             }
-            None => encoded.insert(i, character)
+            None => {
+                encoded.insert(i, character)
+            }
         }
     }
 
